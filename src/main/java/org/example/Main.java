c7cea27a88cda9d42c5316acbbb413aa8fa60297
santos <santoslopez890@gmail.com>
2023-06-30 10:09:26 -0400

    something

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,22 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        public String pay(String name,double hourlyRate,double hoursWorked,double deductionRate){
+            double grossPay;
+            double deduction;
+            double netPay;
+
+            grossPay=hourlyRate*hoursWorked;
+            deduction=grossPay*deductionRate;
+            netPay=grossPay -deduction;
+
+            //"KRIS" ,21.5,10,0.05
+            String result;
+
+            result =name+grossPay+deduction+netPay;
+            return result;
+
+        }
+
     }
 }
